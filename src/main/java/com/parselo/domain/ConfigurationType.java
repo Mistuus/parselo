1e215a6eb6bc1c7202ad07e69ca3831bf9e4508e
patenv <victor@opengamma.com>
2018-08-12 16:00:52 +0100

    Javadoc for ConfigurationType

--- src/main/java/com/parselo/domain/ConfigurationType.java
+++ src/main/java/com/parselo/domain/ConfigurationType.java
@@ -1,16 +1,18 @@
 package com.parselo.domain;
 
 /**
- *
+ * The type of configuration specified by the Parselo annotations.
  */
 public enum ConfigurationType {
 
   /**
-   *
+   * Marks a class to parse where the start & end rows as well as the column names of the Excel will be specified on the
+   * annotation and will never change.
    */
   STATIC,
   /**
-   *
+   * Marks a class where the start & end rows as well as the columns are not known upfront and will be specified using
+   * a ParseloSpec at the time of parsing.
    */
   DYNAMIC
 }
